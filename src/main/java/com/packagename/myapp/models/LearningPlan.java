f116b50c76169f8a2183070204e47ab65e589514
moonica13 <monicaalbu13@gmail.com>
2020-12-10 11:00:36 +0200

    LearningPlan - added academic year, study year, semester, credits

--- src/main/java/com/packagename/myapp/models/LearningPlan.java
+++ src/main/java/com/packagename/myapp/models/LearningPlan.java
@@ -1,5 +1,7 @@
 package com.packagename.myapp.models;
 
+import com.packagename.myapp.models.annotations.Parent;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -18,6 +20,19 @@ public class LearningPlan extends BaseModel {
     @NotNull
     private String name;
 
+    @NotNull
+    private String uni_year;
+
+    @NotNull
+    private int study_year;
+
+    @NotNull
+    private int semester;
+
+    @NotNull
+    private double credits;
+
+    @Parent
     @OneToOne
     private Specialization specialization;
 
@@ -59,6 +74,38 @@ public class LearningPlan extends BaseModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUni_year() {
+        return uni_year;
+    }
+
+    public void setUni_year(String uni_year) {
+        this.uni_year = uni_year;
+    }
+
+    public int getStudy_year() {
+        return study_year;
+    }
+
+    public void setStudy_year(int study_year) {
+        this.study_year = study_year;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
     }
 
     public Specialization getSpecialization() {
